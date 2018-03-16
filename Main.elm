06c3b34461c4426cb4fc1fcf5915442677446ec6
Deepak Duggirala <deepak.duggirala@target.com>
2018-03-16 23:43:05 +0530

    form tutorial

--- Main.elm
+++ Main.elm
@@ -1,33 +1,56 @@
 import Html exposing (text, Html, div, input)
 import Html.Events exposing (onInput)
-import Html.Attributes exposing (placeholder)
+import Html.Attributes exposing (placeholder, type_)
 import Debug exposing (log)
 
 -- MODEL
-type alias Model = String
+type alias Model =
+  {
+    name: String,
+    password: String,
+    passwordAgain: String
+  }
+
+model : Model
+model = Model "" "" ""
 
 
 -- UPDATE
 update : Msg -> Model -> Model
-update msg model = msg
+update msg model =
+  case msg of
+    Name name -> { model | name = name }
+    Password password -> { model | password = password }
+    PasswordAgain passwordAgain -> { model | passwordAgain = passwordAgain }
 
 -- MESSAGE
-type alias Msg = String
+type Msg =
+  Name String
+  | Password String
+  | PasswordAgain String
 
 -- VIEW
 view : Model -> Html Msg
 view model =
   div []
     [
-      input  [placeholder "Text to reverse", onInput identity] [text "+"],
-      div [] [text <| String.reverse model]
+      input  [type_ "text", placeholder "Name", onInput Name] [],
+      input  [type_ "password", placeholder "Password", onInput Password] [],
+      input  [type_ "password", placeholder "Re-enter password", onInput PasswordAgain] [],
+      viewValidation model
     ]
 
+viewValidation : Model -> Html Msg
+viewValidation model =
+  text <| if model.password == model.passwordAgain
+    then "OK"
+    else "Passwords do not match"
+
 --MAIN
-main : Program Never String Msg
+main : Program Never Model Msg
 main =
   Html.beginnerProgram {
-    model = "",
+    model = model,
     view = view,
     update = update
   }
